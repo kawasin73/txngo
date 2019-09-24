06792566d8139da7f0c1e0b036bb0201954d0d92
Kawamura Shintaro <kawasin73@gmail.com>
2019-09-24 16:19:00 +0900

    Split writeSet into recording log buffer (txn.logs) and db cache (txn.writeSet)

--- main.go
+++ main.go
@@ -30,6 +30,52 @@ var (
 	ErrBufferShort = errors.New("buffer size is not enough to deserialize")
 )
 
+type Record struct {
+	Key   string
+	Value []byte
+}
+
+func (r *Record) Serialize(buf []byte) (int, error) {
+	key := []byte(r.Key)
+	value := r.Value
+	total := 5 + len(key) + len(value)
+
+	// check buffer size
+	if len(buf) < total {
+		return 0, ErrBufferShort
+	}
+
+	// serialize
+	// TODO: support NULL value
+	buf[0] = uint8(len(key))
+	binary.BigEndian.PutUint32(buf[1:], uint32(len(r.Value)))
+	copy(buf[5:], key)
+	copy(buf[5+len(key):], r.Value)
+	return total, nil
+}
+
+func (r *Record) Deserialize(buf []byte) (int, error) {
+	if len(buf) < 5 {
+		return 0, ErrBufferShort
+	}
+
+	// parse length
+	keyLen := buf[0]
+	valueLen := binary.BigEndian.Uint32(buf[1:])
+	total := 5 + int(keyLen) + int(valueLen)
+	if len(buf) < total {
+		return 0, ErrBufferShort
+	}
+
+	// copy key and value from buffer
+	r.Key = string(buf[5 : 5+keyLen])
+	// TODO: support NULL value
+	r.Value = make([]byte, valueLen)
+	copy(r.Value, buf[5+keyLen:total])
+
+	return total, nil
+}
+
 type RecordLog struct {
 	Action uint8
 	Record
@@ -78,75 +124,32 @@ func (r *RecordLog) Deserialize(buf []byte) (int, error) {
 	}
 }
 
-type Record struct {
-	Key   string
-	Value []byte
-}
-
-func (r *Record) Serialize(buf []byte) (int, error) {
-	key := []byte(r.Key)
-	value := r.Value
-	total := 5 + len(key) + len(value)
-
-	// check buffer size
-	if len(buf) < total {
-		return 0, ErrBufferShort
-	}
-
-	// serialize
-	buf[0] = uint8(len(key))
-	binary.BigEndian.PutUint32(buf[1:], uint32(len(r.Value)))
-	copy(buf[5:], key)
-	copy(buf[5+len(key):], r.Value)
-	return total, nil
-}
-
-func (r *Record) Deserialize(buf []byte) (int, error) {
-	if len(buf) < 5 {
-		return 0, ErrBufferShort
-	}
-
-	// parse length
-	keyLen := buf[0]
-	valueLen := binary.BigEndian.Uint32(buf[1:])
-	total := 5 + int(keyLen) + int(valueLen)
-	if len(buf) < total {
-		return 0, ErrBufferShort
-	}
-
-	// copy key and value from buffer
-	r.Key = string(buf[5 : 5+keyLen])
-	r.Value = make([]byte, valueLen)
-	copy(r.Value, buf[5+keyLen:total])
-
-	return total, nil
+type RecordCache struct {
+	Record
+	deleted bool
 }
 
 type Txn struct {
 	wal      *os.File
 	db       map[string]Record
-	writeSet map[string]RecordLog
+	logs     []RecordLog
+	writeSet map[string]RecordCache
 }
 
 func NewTxn(wal *os.File) *Txn {
 	return &Txn{
 		wal:      wal,
 		db:       make(map[string]Record),
-		writeSet: make(map[string]RecordLog),
+		writeSet: make(map[string]RecordCache),
 	}
 }
 
 func (txn *Txn) Read(key string) ([]byte, error) {
 	if r, ok := txn.writeSet[key]; ok {
-		if r.Action == LDelete {
+		if r.deleted {
 			return nil, ErrNotExist
 		}
-		switch r.Action {
-		case LInsert, LUpdate:
-			return r.Value, nil
-		default:
-			return nil, fmt.Errorf("unexpected action in writeSet : %v", r.Action)
-		}
+		return r.Value, nil
 	}
 
 	r, ok := txn.db[key]
@@ -157,6 +160,7 @@ func (txn *Txn) Read(key string) ([]byte, error) {
 }
 
 func clone(v []byte) []byte {
+	// TODO: support NULL value
 	v2 := make([]byte, len(v))
 	copy(v2, v)
 	return v2
@@ -165,92 +169,114 @@ func clone(v []byte) []byte {
 func (txn *Txn) Insert(key string, value []byte) error {
 	// check writeSet
 	if r, ok := txn.writeSet[key]; ok {
-		if r.Action != LDelete {
+		if !r.deleted {
 			return ErrExist
 		}
-		// Delete -> Insert == Update
-		// FIXME: Insert -> Delete -> Insert must be Insert (it is invalid)
-		r.Action = LUpdate
-		r.Value = clone(value)
-		txn.writeSet[r.Key] = r
-		return nil
+		// reuse key in writeSet
+		key = r.Key
+	} else {
+		// check that the key not exists in db
+		if _, ok := txn.db[key]; ok {
+			return ErrExist
+		}
+		// reallocate string
+		key = string(key)
 	}
 
-	// check that the key is not exists in db
-	if _, ok := txn.db[key]; ok {
-		return ErrExist
-	}
-
-	// reallocate string
-	key = string(key)
 	// clone value to prevent injection after transaction
 	value = clone(value)
 
-	// add insert action to writeSet
-	txn.writeSet[key] = RecordLog{
-		Action: LInsert,
+	// add to or update writeSet
+	txn.writeSet[key] = RecordCache{
 		Record: Record{
 			Key:   key,
 			Value: value,
 		},
 	}
+
+	// add insert log
+	txn.logs = append(txn.logs, RecordLog{
+		Action: LInsert,
+		Record: Record{
+			Key:   key,
+			Value: value,
+		},
+	})
 	return nil
 }
 
 func (txn *Txn) Update(key string, value []byte) error {
 	// check writeSet
 	if r, ok := txn.writeSet[key]; ok {
-		if r.Action == LDelete {
+		if r.deleted {
 			return ErrNotExist
 		}
-		r.Value = clone(value)
-		txn.writeSet[r.Key] = r
-		return nil
+		// reuse key in writeSet
+		key = r.Key
+	} else {
+		// check that the key exists in db
+		r, ok := txn.db[key]
+		if !ok {
+			return ErrNotExist
+		}
+		// reuse key in db
+		key = r.Key
 	}
 
-	r, ok := txn.db[key]
-	if !ok {
-		return ErrNotExist
-	}
+	// clone value to prevent injection after transaction
+	value = clone(value)
 
-	txn.writeSet[r.Key] = RecordLog{
-		Action: LUpdate,
+	txn.writeSet[key] = RecordCache{
 		Record: Record{
-			Key: r.Key,
-			// clone value to prevent injection after transaction
-			Value: clone(value),
+			Key:   key,
+			Value: value,
 		},
 	}
+
+	// add update log
+	txn.logs = append(txn.logs, RecordLog{
+		Action: LUpdate,
+		Record: Record{
+			Key:   key,
+			Value: value,
+		},
+	})
 	return nil
 }
 
 func (txn *Txn) Delete(key string) error {
 	// check writeSet
 	if r, ok := txn.writeSet[key]; ok {
-		if r.Action == LDelete {
+		if r.deleted {
 			return ErrNotExist
-		} else if r.Action == LInsert {
-			delete(txn.writeSet, key)
-			return nil
 		}
-		r.Value = nil
-		// FIXME: Insert -> Delete will be only Delete (it is invalid)
-		r.Action = LDelete
-		txn.writeSet[r.Key] = r
-		return nil
+		// reuse key in writeSet
+		key = r.Key
+	} else {
+		// check that the key exists in db
+		r, ok := txn.db[key]
+		if !ok {
+			return ErrNotExist
+		}
+		// reuse key in db
+		key = r.Key
 	}
 
-	r, ok := txn.db[key]
-	if !ok {
-		return ErrNotExist
-	}
-
-	txn.writeSet[r.Key] = RecordLog{
-		Action: LDelete,
+	txn.writeSet[key] = RecordCache{
+		deleted: true,
 		Record: Record{
-			Key: r.Key,
+			Key: key,
 		},
 	}
+
+	// add delete log
+	txn.logs = append(txn.logs, RecordLog{
+		Action: LDelete,
+		Record: Record{
+			Key: key,
+		},
+	})
+
 	return nil
 }
 
@@ -264,9 +290,8 @@ func (txn *Txn) Commit() error {
 		buf [4096]byte
 	)
 
-	for _, r := range txn.writeSet {
-		// FIXME: key order in map will be randomized
-		n, err := r.Serialize(buf[i:])
+	for _, rlog := range txn.logs {
+		n, err := rlog.Serialize(buf[i:])
 		if err == ErrBufferShort {
 			// TODO: use writev
 			return err
@@ -297,27 +322,20 @@ func (txn *Txn) Commit() error {
 	}
 
 	// write back writeSet to db (in memory)
-	// FIXME: key order in map will be randomized
-	for key, rlog := range txn.writeSet {
-		switch rlog.Action {
-		case LInsert:
-			txn.db[rlog.Key] = rlog.Record
-
-		case LUpdate:
-			r := txn.db[rlog.Key]
-			r.Value = rlog.Value
-			txn.db[rlog.Key] = r
-
-		case LDelete:
-			delete(txn.db, rlog.Key)
-
-		default:
-			return fmt.Errorf("unexpected action : %v", rlog.Action)
+	for _, r := range txn.writeSet {
+		if r.deleted {
+			delete(txn.db, r.Key)
+		} else {
+			txn.db[r.Key] = r.Record
 		}
 
 		// remove from writeSet
-		delete(txn.writeSet, key)
+		delete(txn.writeSet, r.Key)
 	}
+
+	// clear logs
+	// TODO: clear all key and value pointer and reuse logs memory
+	txn.logs = nil
 
 	return nil
 }
@@ -326,6 +344,7 @@ func (txn *Txn) Abort() {
 	for k := range txn.writeSet {
 		delete(txn.writeSet, k)
 	}
+	txn.logs = nil
 }
 
 func main() {
